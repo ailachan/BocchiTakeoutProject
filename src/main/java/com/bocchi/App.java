6d1b8d12e2bfaa63f6f00cea2d039d5c47d18424
aila <32830121@qq.com>
2023-03-16 17:18:46 +0800

    commit

--- src/main/java/com/bocchi/App.java
+++
@@ -1,8 +0,0 @@
-package com.bocchi;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface App extends BaseMapper<User> {
-}
